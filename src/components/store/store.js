1dcba49b0a99a50c5dbe18233cadd85f84061073
wangjie <11277472@qq.com>
2017-11-21 22:43:13 +0800

    wj 提交store

--- src/components/store/store.js
+++ src/components/store/store.js
@@ -4,11 +4,14 @@ import Vuex from "vuex"
 // import todoList from "../todoList/store.js"
 // import counter from "../counter/store.js"
 // import empStore from "../app/info/store/empStore.js"
+import newsListStore from "../../components/app/info/news/newsListStore.js"
+import addNewsStore from "../../components/app/info/news/addNewsStore.js"
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
 	modules: {
-	 
+		newsListStore,	
+		addNewsStore, 
 	}
 })
