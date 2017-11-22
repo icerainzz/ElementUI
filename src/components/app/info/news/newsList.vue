af392606be58589e5559ddab3db2a4a5cb127954
wangjie <11277472@qq.com>
2017-11-22 15:47:42 +0800

    wj 增删改查做完，分页没有做完

--- src/components/app/info/news/newsList.vue
+++ src/components/app/info/news/newsList.vue
@@ -1,42 +1,66 @@
 <template>
 	<div>
-		<h1>资讯列表</h1>
-		<el-table
-			:data="data"
-	      	style="width: 700px;">
-	      	<el-table-column
-		        prop="_id"
-		        label="编号"
-		        width="180">
-		    </el-table-column>
-	      	<el-table-column
-		        prop="name"
-		        label="标题"
-		        width="180">
-		    </el-table-column>
-	    	<el-table-column
-		        prop="newsCnt"
-		        label="内容">
-		      </el-table-column>
-	      	<el-table-column
-		      	fixed="right"
-		      	label="操作"
-		     	 width="100">
-		      	<template slot-scope="scope">
-		        	<el-button type="text" @click="handleClick(scope.row)" size="small">修改</el-button>
-		        	<el-button type="text" size="small">编辑</el-button>
-		      	</template>
-		    </el-table-column>
-	    </el-table>
-	    <el-pagination
-	      	@size-change="handleSizeChange"
-	      	@current-change="handleCurrentChange"
-	      	:current-page="~~curPage"
-	      	:page-sizes="[20, 40, 80, 100]"
-	      	:page-size="~~eachPage"
-	      	layout="total, sizes, prev, pager, next, jumper"
-	      	:total="count">
-	    </el-pagination>
+		<div>
+			<h1>资讯列表</h1>
+			<el-table
+				:data="data"
+		      	style="width: 700px;">
+		      	<el-table-column
+			        prop="_id"
+			        label="编号"
+			        width="180">
+			    </el-table-column>
+		      	<el-table-column
+			        prop="name"
+			        label="标题"
+			        width="180">
+			    </el-table-column>
+		    	<el-table-column
+			        prop="newsCnt"
+			        label="内容">
+			      </el-table-column>
+		      	<el-table-column
+			      	fixed="right"
+			      	label="操作"
+			     	 width="100">
+			      	<template slot-scope="scope">
+			        	<el-button type="text" @click="amendClick(scope.row)" size="small">修改</el-button>
+			        	<el-button type="text" size="small" @click="deleteClick(scope.row)">编辑</el-button>
+			      	</template>
+			    </el-table-column>
+		    </el-table>
+		    <el-pagination
+		      	@size-change="handleSizeChange"
+		      	@current-change="handleCurrentChange"
+		      	:current-page="~~curPage"
+		      	:page-sizes="[20, 40, 80, 100]"
+		      	:page-size="~~eachPage"
+		      	layout="total, sizes, prev, pager, next, jumper"
+		      	:total="count">
+		    </el-pagination>
+		</div>
+
+	    <div id="amend" class="amend" style="display: none; margin-top: 30px">
+			标题：<el-input
+			  	type="textarea"
+			  	autosize
+			  	placeholder="请输入标题"
+			  	v-model="textarea2" ref="todo">
+			</el-input>
+			
+			<div style="margin: 20px 0;"></div>
+
+			内容：<el-input
+			  	type="textarea"
+			 	 :autosize="{ minRows: 2, maxRows: 4}"
+			 	 placeholder="请输入内容"
+			 	 v-model="textarea3" ref="todo2">
+			</el-input>
+			<div style="margin-top: 20px">
+				<el-button type="primary" round @click="amendSave">保存</el-button>
+				<el-button type="primary" round @click="amendReset">重置</el-button>
+			</div>
+		</div>
 	</div>
 </template>
 <script>
@@ -48,12 +72,43 @@
 			this.getNewssAsync()
 		},
 		computed: {
-			...mapState("newsListStore", ["curPage", "eachPage", "maxPage", "count", "data"])
+			// 提取后台数据
+			...mapState("newsListStore", ["curPage", "eachPage", "maxPage", "count", "data"]),
+			// 修改数据并保存
+			...mapState("newsListStore", ["name", "newsCnt", "_id"]),
+			// 删除数据
+			...mapState("newsListStore", "_id")
 		},
 		methods: {
-			handleClick(row) {
-				// console.log(row)
+			amendClick(row) {
+				document.getElementById("amend").style.display="block";
+				this.$data.textarea2 = row.name,
+				this.$data.textarea3 = row.newsCnt,
+				this.$data._id = row._id
 			},
+			// 修改后保存
+			amendSave(row) {
+				const newsData = {
+					name: this.$data.textarea2,
+					newsCnt: this.$data.textarea3,
+					_id: this.$data._id
+				}  
+				this.addAnewsAsync(newsData)
+				this.getNewssAsync()
+				document.getElementById("amend").style.display="none";
+
+	      	},
+	      	amendReset() {
+	      		// console.log("on");
+	      	},
+
+	      	// 删除
+	      	deleteClick(row) {
+	      		this.delateAsync(row)
+	      		this.getNewssAsync()
+	      	},
+
+	      	// 分页
 			handleSizeChange(val) {
 	        	console.log(`每页 ${val} 条`);
 	        	this.getNewssAsync({
@@ -68,7 +123,12 @@
 	        		eachPage: this.eachPage
 	        	})
 	      	},			
-			...mapActions("newsListStore", ["getNewssAsync"])
+				// 提取数据渲染到列表		
+			...mapActions("newsListStore", ["getNewssAsync"]),
+				// 修改后保存
+			...mapActions("newsListStore", ["addAnewsAsync"]),
+				// 删除数据
+			...mapActions("newsListStore", ["delateAsync"])
 		}
 	}
 </script>
--- src/components/app/info/news/newsListStore.js
+++ src/components/app/info/news/newsListStore.js
@@ -9,15 +9,25 @@ export default {
 		data: []		
 	},
 	mutations: {
+		// 查询数据，修改数据结构的赋值情况
 		getNewss(state, page) {
 			state.data = page.rows
 			state.count = page.total
 			state.maxPage = Math.ceil(state.count/state.eachPage)
-			console.log(state);
-
+		},
+		// 修改数据 
+		addAnews(state, page){
+			// console.log(state)
+			// console.log(page);
+		},
+		delate(state, page) {
+			// console.log(page);
+			// console.log(state);
 		}
+
 	},
 	actions: {
+		// 查询news数据并渲染
 		async getNewssAsync(context, {curPage = this.state.newsListStore.curPage, eachPage = this.state.newsListStore.eachPage} = {}) {
 			const {data} = await axios.get("/news/getNewss", {
 				params: {
@@ -26,6 +36,16 @@ export default {
 				}
 			})
 			context.commit("getNewss", data)
+		},
+		// 修改news数据 
+		async addAnewsAsync(context, page) {
+			const data = await axios.post("/news/addAnews", page)
+			context.commit("addAnews", data)
+		},
+		// 删除news数据
+		async delateAsync(context, page) {
+			const data = await axios.post("/news/delate", page)
+			context.commit("delate", data)
 		}
 	}
 }
